6a2605beb9e29a3631b27efa65fcc1f92a49aa7f
Frank Seidinger <fs@frank-seidinger.de>
2024-05-30 21:22:11 +0200

    Added  reusable code for window contact handling and heating controller lock

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { actions, time, items } = require('openhab');
+
 // Format a numeric value to a fixed point quantity
 function formatQuantity(value, fixedPoints, unitOfMeasurement) {
     assertNumber(value);
@@ -48,6 +50,98 @@ function assertNumber(value) {
     }
 }
 
+/*
+    The event when triggered by UI is like this:
+
+    {
+        "eventClass": "org.openhab.core.automation.events.ExecutionEvent",
+        "payload": {},
+        "eventType": "manual"
+    }
+
+    The event when triggered by item state change is like this:
+
+    {
+        "receivedState": "65 %",
+        "eventClass": "org.openhab.core.items.events.ItemStateUpdatedEvent",
+        "payload": {
+            "type": "Quantity",
+            "value": "65 %"
+        },
+        "itemName": "Badezimmer_Klima_Luftfeuchtigkeit",
+        "eventType": "update",
+        "triggerType": "ItemStateUpdateTrigger",
+        "module": "795328d5-04c2-441b-96c8-5f1eef990eb8"
+    }  
+*/
+
+function enhanceEvent(event, itemName, logEvent = false) {
+    if (event.eventType === "manual") {
+        const item = items.getItem(itemName)
+        event.itemName = item.name
+        event.payload = {
+            "type" : item.type,
+            "value": item.state
+        }
+
+        console.info("Enhanced event due to manual call")
+    }
+
+    if (logEvent) {
+        console.info("Event is", event)
+    }
+
+    return event
+}
+
+function processWindowContactForHeatingLock(event, context, windowContact, heatingLock) {
+    const payload = event.payload
+
+    if (payload.value === "OPEN") {
+        // Reset old timer after window was opened
+        resetTimer(context, windowContact, heatingLock)
+
+        // Schedule a new timer
+        scheduleTimer(context, windowContact, heatingLock)
+    } else {
+        // Reset timer after window was closed
+        resetTimer(context, windowContact, heatingLock)
+    }
+}
+
+function scheduleTimer(context, windowContact, heatingLock) {
+    var scheduledTime = time.ZonedDateTime.now().plusSeconds(180);
+  
+    context.timer = actions.ScriptExecution.createTimer(scheduledTime, function() {
+        switchOffHeating(context, windowContact, heatingLock);
+    });
+
+    console.debug("Scheduled timer for", windowContact.name)
+}
+
+function resetTimer(context, windowContact, heatingLock) {
+    if (typeof context.timer !== 'undefined') {
+        context.timer.cancel()
+        context.timer = undefined
+
+        console.debug("Cancelled timer for", windowContact.name)
+    }
+
+    heatingLock.sendCommand("OFF")
+    console.info("Switched off heating lock", heatingLock.name)
+}
+
+function switchOffHeating(context, windowContact, heatingLock) {
+    if (windowContact.state === "NULL" || windowContact.state === "OPEN") {
+        heatingLock.sendCommand("ON")
+        console.info("Switched on heating lock", heatingLock.name)
+    } else {
+        console.info("Switched off heating lock", heatingLock.name, "due to window closing during grace time period")
+    }
+    
+    context.timer = undefined
+}
+
 module.exports = {
     formatQuantity,
     formatPercentage,
@@ -55,5 +149,7 @@ module.exports = {
     setItemState,
     isExecutedBySource,
     assertNotUndefinedOrNull,
-    assertNumber
+    assertNumber,
+    enhanceEvent,
+    processWindowContactForHeatingLock
 };
